542003995f9cfedf5aebc21c477da10c3c6a5603
menaujiri <ujirimena>
2024-01-24 16:06:17 -0800

    Safe_print_division file

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 def safe_print_division(a, b):
-    """Returns the division of a by b."""
     try:
-        div = a / b
-    except (TypeError, ZeroDivisionError):
-        div = None
+        answer = a / b
+    except (ZeroDivisionError, FloatingPointError):
+        answer = None
     finally:
-        print("Inside result: {}".format(div))
+        print("Inside result: {}".format(answer))
+    return answer
