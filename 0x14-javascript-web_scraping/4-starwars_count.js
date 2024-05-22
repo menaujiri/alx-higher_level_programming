3c48559bb7e4b22a28019cf58aa6a2eaca0666a5
menaujiri <ujirimena@gmail.com>
2024-05-22 04:06:44 +0000

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 const url = process.argv[2];
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
-  } else {
-    const films = JSON.parse(body).results;
-    const characterId = '18';
-    const count = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
-    console.log(count);
+request(url, (error, response, body) => {
+  if (error) { console.log(error); }
+  const jsonBody = JSON.parse(body);
+  let wedgeCount = 0;
+  for (const result of jsonBody.results) {
+    for (const charURL of result.characters) {
+      if (charURL.includes(18)) {
+        wedgeCount++;
+      }
+    }
   }
+  console.log(wedgeCount);
 });
